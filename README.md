394205de62e61467d3c9f2a1a7eff88f7e20d780
Jeremy The SuperGeek <tacoman667@gmail.com>
2012-10-18 17:43:11 -0500

    ready for public consumption

---
+++ README.md
@@ -0,0 +1,16 @@
+This is just a little website that will serve up a static html page that I use for hosting my resume on heroku.
+
+It will serve up links for an rtf version and a pdf version. Should someone click those links, an email will go out to the owner of this site as defined in an emailServerOptions.json file. The email will contain a url to a free site that will serve up information about the IP address from whom downloaded the resume.
+
+Contents of the emailServerOptions.json file:
+
+{
+	"host" : "smtp.example.com",
+    "port" : "25",             
+    "domain" : "smtp.example.com",
+    "to" : "sample@example.com",
+    "from" : "sample@example.com",
+    "authentication" : "login",
+    "username" : "sample@example.com",
+    "password" : "p@ssw0rd"
+}
---
+++ web.js
@@ -0,0 +1,105 @@
+var express = require('express');
+var fs = require('fs');
+var jQuery = require('jquery');
+
+
+var app = express.createServer(express.logger());
+
+app.use('/public', express.static(__dirname + "/public"));
+app.use('/images', express.static(__dirname + "/images"));
+
+app.get('/', function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var contents = fs.readFileSync("./public/resume.html", "UTF-8");
+  res.end(contents);
+  //sendEmail(getClientIp(req));
+});
+
+app.get('/:ext', function(req, res) {
+  var extension = req.params.ext;
+  if (extension === 'favicon.ico') { return; }
+
+  var filename = "resume." + extension;
+  
+  switch (extension) {
+    case 'pdf':
+      res.sendfile('./public/' + filename);
+      sendEmail(getClientIp(req), "Someone downloaded your resume in " + extension + " format.");
+      break;
+    case 'rtf':
+      res.download('./public/' + filename, filename);
+      sendEmail(getClientIp(req), "Someone downloaded your resume in " + extension + " format.");
+      break;
+    default:
+      break;
+  }
+});
+
+function sendEmail(ip_address, subject) {
+  //http://whatismyipaddress.com/ip/<ip_address>
+  var msg = 'Location: http://whatismyipaddress.com/ip/' + ip_address;
+  sendEmailWithData(msg, subject);
+}
+
+// composes the email using data for the email body and sends it
+function sendEmailWithData(data, subject) {
+  if (!subject) {
+    subject = "Someone looked at your resume at jeremyhardin.com";
+  }
+  var email = require('mailer');
+
+  var options = getEmailOptions(data, subject);
+
+  email.send(
+    options,
+    function(err, result){
+      if(err){ console.log(err); }
+  });
+}
+
+
+// composes the options to be used for sending an email
+function getEmailOptions(data, subject) {
+  var sendOptions = {
+    subject : subject,
+    body: "Your resume was just viewed at jeremyhardin.com!\n" + data,
+    callback: function(err, data) {
+      if (!err) {
+        console.log("email failed!");
+      } else {
+        console.log("email sent!");
+      }
+    }
+  };
+
+  var emailServerOptions = fs.readFileSync("./emailServerOptions.json", "UTF-8");
+  var options = jQuery.extend(JSON.parse(emailServerOptions), sendOptions);
+
+  return options;
+}
+
+// gets the client ip address
+function getClientIp(req) {
+  var ipAddress;
+  // Amazon EC2 / Heroku workaround to get real client IP
+  var forwardedIpsStr = req.header('x-forwarded-for'); 
+  if (forwardedIpsStr) {
+    // 'x-forwarded-for' header may return multiple IP addresses in
+    // the format: "client IP, proxy 1 IP, proxy 2 IP" so take the
+    // the first one
+    var forwardedIps = forwardedIpsStr.split(',');
+    ipAddress = forwardedIps;
+  }
+  if (!ipAddress) {
+    // Ensure getting client IP address still works in
+    // development environment
+    ipAddress = req.connection.remoteAddress;
+  }
+  return ipAddress;
+};
+
+// starts the server
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
